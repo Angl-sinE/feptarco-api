004f8f25a009830df177fc4d647d907b75e40ac9
AngSine <anglflores.sine@gmail.com>
2019-09-02 21:19:49 -0500

    Adding package and fixing index

---
+++ app/Http/Controllers/API/TourneyController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user1
+ * Date: 1/09/2019
+ * Time: 18:37
+ */
+
+namespace App\Http\Controllers\API;
+
+
+class TourneyController extends ApiController
+{
+
+
+    public function register()
+    {
+
+    }
+
+    public function downloadFile()
+    {
+
+    }
+
+}
--- public/index.php
+++ public/index.php
@@ -7,10 +7,7 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-define('LARAVEL_START', microtime(true));
-    header('Access-Control-Allow-Origin', '*');
-    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
