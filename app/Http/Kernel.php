09c0cdc7868a650e798b2038ca8a74ff988d437d
AngSine <anglflores.sine@gmail.com>
2019-09-15 13:37:28 -0500

    Fixing kernel http config

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,6 @@ class Kernel extends HttpKernel
         'api' => [
             \Illuminate\Session\Middleware\StartSession::class,
             \App\Http\Middleware\EncryptCookies::class,
-            \Barryvdh\Cors\ServiceProvider::class,
             'throttle:60,1',
             'bindings',
         ],
